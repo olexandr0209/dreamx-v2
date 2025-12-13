f144f587835e405ed12958f973ef703a809ac2fa
olexandr0209 <olexandrkrychylskiy@gmail.com>
2025-12-13 12:24:08 +0100

    Update main.py

--- bots/game_bot/main.py
+++ bots/game_bot/main.py
@@ -1,1 +1,17 @@
+import asyncio
+from aiogram import Bot, Dispatcher
+from handlers.start import router
+import os
+
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+
+async def main():
+    bot = Bot(token=BOT_TOKEN)
+    dp = Dispatcher()
+    dp.include_router(router)
+
+    await dp.start_polling(bot)
+
+if __name__ == "__main__":
+    asyncio.run(main())
 
