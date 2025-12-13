7de8145ee67e9d61b82c068369d8cd5755e75847
olexandr0209 <olexandrkrychylskiy@gmail.com>
2025-12-13 13:43:04 +0100

    Update home_screen.js

--- webapp/src/js/home_screen.js
+++ webapp/src/js/home_screen.js
@@ -1,1 +1,10 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const btn = document.getElementById("btn-play-bot");
+  if (!btn) return;
+
+  btn.addEventListener("click", () => {
+    // Поки просто відкриваємо екран гри проти бота
+    window.location.href = "./game.html?mode=bot";
+  });
+});
 
