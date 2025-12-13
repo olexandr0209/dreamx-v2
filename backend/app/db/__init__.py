ddc469e9981052fbb6c1c79801a3e522b6ddf72b
olexandr0209 <olexandrkrychylskiy@gmail.com>
2025-12-13 10:39:51 +0100

    Update __init__.py

--- backend/app/db/__init__.py
+++ backend/app/db/__init__.py
@@ -1,1 +1,5 @@
+from .db import fetch_one, fetch_all, execute, execute_returning_one
+from .connection import get_conn
+
+__all__ = ["get_conn", "fetch_one", "fetch_all", "execute", "execute_returning_one"]
 
