91009f7dd2cf06074545bc30ae3b42c2052787e2
olexandr0209 <olexandrkrychylskiy@gmail.com>
2025-12-13 15:46:09 +0100

    Update app.py

--- backend/app/app.py
+++ backend/app/app.py
@@ -5,14 +5,16 @@ from flask import Flask, jsonify
 from app.api.players import bp_players
 from app.api.games import bp_games
 #from app.api.users import bp_users   якщо є
-#from app.api.health import bp_health  # якщо є
-#from app.api.tournaments import bp_tournaments  # якщо є
+from app.api.routes_health import bp_health  # якщо є
+from app.api.routes_tournaments import bp_tournaments  # якщо є
+
+
 
 def create_app():
     app = Flask(__name__)
 
-    #app.register_blueprint(bp_health)
-    #app.register_blueprint(bp_tournaments)
+    app.register_blueprint(bp_health)
+    app.register_blueprint(bp_tournaments)
     app.register_blueprint(bp_players)
     #app.register_blueprint(bp_users)
     app.register_blueprint(bp_games)
