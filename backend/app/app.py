bbbaba57d10477e9e6a44b5b92201fa4d5fe950e
olexandr0209 <olexandrkrychylskiy@gmail.com>
2025-12-13 09:17:07 +0100

    Update app.py

--- backend/app/app.py
+++ backend/app/app.py
@@ -1,18 +1,13 @@
 from flask import Flask, jsonify
 from .routes_health import bp_health
-from .routes_users import bp_users
-from .routes_tournaments import bp_tournaments
 
 def create_app():
     app = Flask(__name__)
 
     app.register_blueprint(bp_health)
-    app.register_blueprint(bp_users)
-    app.register_blueprint(bp_tournaments)
 
     @app.errorhandler(Exception)
     def handle_error(e):
-        # щоб фронт бачив JSON навіть при помилках
         return jsonify({"ok": False, "error": str(e)}), 500
 
     return app
