510123940ef885c67b0b0fd1a8013199ab1dd6dc
olexandr0209 <olexandrkrychylskiy@gmail.com>
2025-12-13 09:27:28 +0100

    Update app.py

--- backend/app/app.py
+++ backend/app/app.py
@@ -1,10 +1,12 @@
 from flask import Flask, jsonify
 from .routes_health import bp_health
+from .routes_tournaments import bp_tournaments
 
 def create_app():
     app = Flask(__name__)
 
     app.register_blueprint(bp_health)
+    app.register_blueprint(bp_tournaments)
 
     @app.errorhandler(Exception)
     def handle_error(e):
