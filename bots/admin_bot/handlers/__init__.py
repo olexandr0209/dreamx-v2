09acb591eb4597c81623b91619e60dea504f7cc5
olexandr0209 <olexandrkrychylskiy@gmail.com>
2025-12-17 12:06:41 +0100

    Update __init__.py

--- bots/admin_bot/handlers/__init__.py
+++ bots/admin_bot/handlers/__init__.py
@@ -1,1 +1,9 @@
+from aiogram import Router
+
+from .settings import router as settings_router
+from .creators import router as creators_router
+
+router = Router()
+router.include_router(settings_router)
+router.include_router(creators_router)
 
