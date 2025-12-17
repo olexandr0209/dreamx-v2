e08e95ae7e61cbee49facb5db3df68502ca1ef1f
olexandr0209 <olexandrkrychylskiy@gmail.com>
2025-12-17 14:55:39 +0100

    Create rules.py

---
+++ backend/app/engine/tournament_groups/rules.py
@@ -0,0 +1,11 @@
+# backend/app/engine/tournament_groups/rules.py
+
+MOVES = ("rock", "paper", "scissors")
+
+# Серія в матчі: 9 ігор (3 раунди * 3 гри)
+SERIES_GAMES_TOTAL = 9
+
+# Очки за 1 гру
+POINTS_WIN = 3
+POINTS_DRAW = 1
+POINTS_LOSE = 0
